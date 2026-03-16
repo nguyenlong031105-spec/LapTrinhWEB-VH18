3c9532f7c9522286dffac1cf0164e4e6de662520
Nguyen Long <nguyenlong031105@gmail.com>
2026-03-16 15:13:03 +0700

    laptrinhweb

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
+import anh from '..l/public/deptrai.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <image src={anh} alt="deptrai" width={1000} height={1000}/>
+    </>git init
+
+
   );
 }
 
